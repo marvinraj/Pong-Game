b55dae25aad94d40fa8fe85502e03a08541354f8
Marvin <marvinraj77@gmail.com>
2023-10-18 18:54:20 +0800

    added ball class

--- main.py
+++ main.py
@@ -3,48 +3,30 @@ import sys
 import random
 from pygame.math import Vector2
 
-
 class BALL:
     def __init__(self):
-        # create x and y position
-        self.x = 5
-        self.y = 4
-        self.x_speed = 5
-        self.y_speed = 4
-        self.direction = 1
-        self.pos = Vector2(self.x, self.y)
+        self.ball_speed_x = random.randint(1,4)
+        self.ball_speed_y = random.randint(1,4)
+        self.ball_pos_x = 300
+        self.ball_pos_y = 300
+        
 
-    # draw a square 
     def draw_ball(self):
-        # create rect
-        ball_rect = pygame.Rect(self.pos.x*cell_size, self.pos.y*cell_size, cell_size, cell_size)
+        # create ball
+        ball1 = pygame.draw.circle(screen, (42,95,23), [self.ball_pos_x,self.ball_pos_y], 5, 0)
 
+        ball = ball1.move(self.ball_speed_x, self.ball_speed_y)
         # move rect
-        # left right
-        if ball_rect.left <= 20 or ball_rect.right >= 580:
-            self.direction *= -1
-            self.x_speed = random.randint(0,8) * self.direction
-            self.y_speed = random.randint(0,8) * self.direction
+        if ball.left <=0 or ball.right >=600:
+            self.ball_speed_x = -self.ball_speed_x
+        if ball.top <=0 or ball.top >=600:
+            self.ball_speed_y= -self.ball_speed_y
+        
+        self.ball_pos_x += self.ball_speed_x
+        self.ball_pos_y += self.ball_speed_y
 
-            if self.x_speed == 0 and self.y_speed == 0:
-                self.x_speed = random.randint(2,8) * self.direction
-                self.y_speed = random.randint(2,8) * self.direction
-        # top bottom
-        if ball_rect.top <= 20 or ball_rect.bottom >= 580:
-            self.direction *= -1
-            self.x_speed = random.randint(0,8) * self.direction
-            self.y_speed = random.randint(0,8) * self.direction
-
-            if self.x_speed == 0 and self.y_speed == 0:
-                self.x_speed = random.randint(2,8) * self.direction
-                self.y_speed = random.randint(2,8) * self.direction
-
-        ball_rect.left += self.x_speed
-        ball_rect.top += self.y_speed
-
-        # draw rect
-        pygame.draw.rect(screen, (124,165,112),ball_rect) #surface, color, rect
-
+        # draw ball
+        pygame.draw.circle(screen, (42,95,23), ball1.center, 5, 0)
 
 pygame.init()
 cell_size = 30
@@ -66,12 +48,7 @@ u2_height = 50
 u2_speed = 5
 
 #ball
-
-ball_speed_x = 3
-ball_speed_y = 3
-ball_pos_x = 300
-ball_pos_y = 300
-ball = pygame.draw.circle(screen, (42,95,23), [ball_pos_x,ball_pos_y], 5, 0)
+ball = BALL()
 
 
 while True:
@@ -91,12 +68,6 @@ while True:
         u2_y-=u2_speed
     if keys[pygame.K_s] and u2_y<(cell_number*cell_size)-u2_height:
         u2_y+=u2_speed
-    # ball
-    ball = ball.move(ball_speed_x, ball_speed_y)
-    if ball.left <=0 or ball.right >=600:
-        ball_speed_x = -ball_speed_x
-    if ball.top <=0 or ball.top >=600:
-        ball_speed_y= -ball_speed_y
 
     # color for window background
     screen.fill((34,3,3))
@@ -105,8 +76,8 @@ while True:
     # draw the paddle 2
     pygame.draw.rect(screen, (12,26,116), (u2_x,u2_y,u2_width,u2_height))
     # draw ball
-    pygame.draw.circle(screen, (42,95,23), ball.center, 5, 0)
-    print(ball.center)
+    ball.draw_ball()
+    
 
     pygame.display.update()
     clock.tick(60)
